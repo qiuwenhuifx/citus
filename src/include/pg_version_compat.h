bc7f6bc67d0d2beb99c021d3160158ad8675a8a2
onderkalaci <onderkalaci@gmail.com>
2023-04-26 22:30:28 +0300

    Change in Var representations, get_relids_in_jointree
    PG commit
    https://github.com/postgres/postgres/commit/2489d76c4906f4461a364ca8ad7e0751ead8aa0d
    2489d76c4906f4461a364ca8ad7e0751ead8aa0d

--- src/include/pg_version_compat.h
+++ src/include/pg_version_compat.h
@@ -40,6 +40,8 @@ get_guc_variables_compat(int *gucCount)
 #define stringToQualifiedNameList_compat(a, b) stringToQualifiedNameList(a, b)
 #define typeStringToTypeName_compat(a, b) typeStringToTypeName(a, b)
 
+#define get_relids_in_jointree_compat(a, b, c) get_relids_in_jointree(a, b, c)
+
 #else
 
 #include "storage/relfilenode.h"
@@ -70,6 +72,8 @@ get_guc_variables_compat(int *gucCount)
 
 #define stringToQualifiedNameList_compat(a, b) stringToQualifiedNameList(a)
 #define typeStringToTypeName_compat(a, b) typeStringToTypeName(a)
+
+#define get_relids_in_jointree_compat(a, b, c) get_relids_in_jointree(a, b)
 
 #endif
 
